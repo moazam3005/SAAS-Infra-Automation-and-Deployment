fbc7ed2f4926bde7be65e91aa5416cd4181e9a88
moazam3005 <98914912+moazam3005@users.noreply.github.com>
2025-08-21 22:42:24 +0500

    Add files via upload

---
+++ infrastructure/iam/variables.tf
@@ -0,0 +1,5 @@
+variable "project_name" { type = string }
+variable "artifact_bucket_arn" { type = string }
+variable "github_org"  { type = string }
+variable "github_repo" { type = string }
+variable "tags"        { type = map(string) }
