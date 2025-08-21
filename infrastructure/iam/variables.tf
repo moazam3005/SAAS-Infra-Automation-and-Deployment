3785e7b47376c5559034ab55822d613b253b97f8
moazam3005 <98914912+moazam3005@users.noreply.github.com>
2025-08-21 22:43:29 +0500

    Delete infrastructure/iam directory

--- infrastructure/iam/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "project_name" { type = string }
-variable "artifact_bucket_arn" { type = string }
-variable "github_org"  { type = string }
-variable "github_repo" { type = string }
-variable "tags"        { type = map(string) }
